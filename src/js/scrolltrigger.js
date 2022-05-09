24111649a60b5fbab3cbfbec437d821c88c2a4c1
Samuel Siau <samuelsiau@gmail.com>
2022-05-09 22:39:35 +1200

    Updated

--- src/js/scrolltrigger.js
+++ src/js/scrolltrigger.js
@@ -2,18 +2,21 @@ import { pageTransitionsBarba } from './libs/pageTransitionsBarba';
 import barba from '@barba/core';
 import gsap from 'gsap';
 import scrollTrigger from 'gsap/ScrollTrigger';
+import '../styles/scrolltrigger.module.scss';
 
 pageTransitionsBarba();
 
 gsap.registerPlugin(scrollTrigger);
 
+gsap.set('.box', {x:500})
+
 gsap.to(".box", {
   scrollTrigger: {
-    trigger: this,
+    trigger: '.box',
     markers: true,
-    start: "center 80%",
-    end: "bottom 100px",
-    toggleActions: "restart complete restart complete"
+    toggleClass: 'active',
+    toggleActions:"play none none none"
   },
-  x: 100,
+  x: 0
+  // opacity: 1
 });
